ff8e518cd80115e841133a29a5b32798f81637cb
Jose Garcia <josegarcia19809@gmail.com>
2026-02-08 13:07:15 -0600

    Variables: lista de compras

---
+++ 01_fundamentos/lista_compras.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Jose Garcia on 08/02/26.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declaración de variables
+    int manzanas, leche, cereal;
+    manzanas = 3;
+    leche = 2;
+    cereal = 5;
+
+    // Salida en pantalla
+    cout << "******************************" << endl;
+    cout << "🛍️ Lista de compras:\n" << endl;
+    cout << "🍎 Traer " << manzanas << " manzanas" << endl;
+    cout << "🥛 Comprar " << leche << " litros de leche" << endl;
+    cout << "🥣 También " << cereal << " cajas de cereal" << endl;
+    cout << "******************************" << endl;
+
+    return 0;
+}
