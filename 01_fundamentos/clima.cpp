dbb509e0e2f0594857c5cc8248b43a1fff3ee3d7
josegarcia19809 <josegarcia19809@gmail.com>
2026-02-09 08:43:14 -0600

    Uso de variables: clima, fruteria

---
+++ 01_fundamentos/clima.cpp
@@ -0,0 +1,21 @@
+// Creado por José Luis García
+// 9 febrero de 2026
+// Programa para mostrar datos del clima
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int dia = 1;
+    float temperatura = 29.6;
+    float viento = 12.8;
+
+    cout << "*********************" << endl;
+    cout << "Registro del clima" << endl<<endl;
+    cout << "Día del mes: " << dia << endl;
+    cout << "Temperatura: " << temperatura << "ºC" << endl;
+    cout << "Velocidad del viento: " << viento << " km/h" << endl;
+    cout << "*********************" << endl;
+
+    return 0;
+}
---
+++ 01_fundamentos/fruteria.cpp
@@ -0,0 +1,20 @@
+// Creado por José Luis García
+// 9 febrero de 2026
+// Programa para mostrar un pequeño resumen de compra de fruta
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int platanos = 6;
+    int manzanas = 4;
+    double precioKilo = 28.50;
+
+    cout << "**********************" << endl;
+    cout << "Compra en la frutería" << endl;
+    cout << "Comprar " << platanos << " plátanos" << endl;
+    cout << "Comprar " << manzanas << " manzanas" << endl;
+    cout << "Precio por kilo $" << precioKilo << endl;
+    cout << "**********************" << endl;
+    return 0;
+}
