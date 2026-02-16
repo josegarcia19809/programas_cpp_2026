92f96d9a9cfad9f7fc92a150715bac494ebf1595
josegarcia19809 <josegarcia19809@gmail.com>
2026-02-16 11:29:19 -0600

    Entrada de datos: pagar producto

---
+++ 01_fundamentos/pagar_producto.cpp
@@ -0,0 +1,23 @@
+// Elaborado por José L García
+// Fecha: 16 febrero de 2026
+// Programa para calcular el total de un producto
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double precio;
+    int cantidad;
+    double total;
+
+    cout << "Ingrese el precio del producto: ";
+    cin >> precio;
+
+    cout << "Ingresa la cantidad comprada: ";
+    cin >> cantidad;
+
+    total = precio * cantidad;
+    cout << "El total a pagar es: " << total << endl;
+
+    return 0;
+}
